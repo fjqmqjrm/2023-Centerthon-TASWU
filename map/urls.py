87e868d128b81a9a69e846184e2c123027cab094
fjqmqjrm <fjqmqjrm@naver.com>
2023-07-30 03:42:30 +0900

    feat: KakaoLogin+유저구분

--- map/urls.py
+++ map/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
-
+app_name = 'map'
 urlpatterns = [
     path('', views.map_main, name='map_main'),
-    path('custom_logout/', views.custom_logout, name='custom_logout'),
+    #path('custom_logout/', views.custom_logout, name='custom_logout'),
 ]
--- profiles/models.py
+++ profiles/models.py
@@ -8,6 +8,6 @@ class UserProfile(models.Model):
     # null=True를 설정하여 UserProfile과 User의 1대1 관계에서 UserProfile이 없어도 됨을 나타냄
     # 여기에 원하는 추가 정보 필드들을 정의합니다
     # 예를 들어, 닉네임, 프로필 사진, 연락처 등
-
+    is_taxi_driver = models.BooleanField(default=False)
     def __str__(self):
         return self.user.username
